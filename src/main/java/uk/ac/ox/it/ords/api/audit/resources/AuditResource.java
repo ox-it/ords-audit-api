be9ae462c7daed200fd1282cf3f14d74df40259d
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-12-18 15:09:48 +0000

    Added Swagger documentation generation

--- src/main/java/uk/ac/ox/it/ords/api/audit/resources/AuditResource.java
+++ src/main/java/uk/ac/ox/it/ords/api/audit/resources/AuditResource.java
@@ -15,6 +15,16 @@
  */
 package uk.ac.ox.it.ords.api.audit.resources;
 
+import java.net.URI;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -30,8 +40,24 @@ import uk.ac.ox.it.ords.api.audit.permissions.AuditPermissions;
 import uk.ac.ox.it.ords.security.model.Audit;
 import uk.ac.ox.it.ords.security.services.AuditService;
 
+@Api(value="Audit")
 public class AuditResource {
 
+	@ApiOperation(value="Creates a new audit entry")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 201, message = "Audit record successfully created.",
+					responseHeaders = {@ResponseHeader(name = "Location", description = "The URI of the record", response = URI.class)}
+					),
+		    @ApiResponse(code = 400, message = "Invalid Project."),
+		    @ApiResponse(code = 403, message = "Not authorized to create an audit record.")
+	})
+	// 
+	// These are used by upstream gateways; including them here makes it easier to use an API portal
+	//
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "Version", value = "API version number", required = false, dataType = "string", paramType = "header"),
+	    @ApiImplicitParam(name = "Authorization", value = "API key", required = false, dataType = "string", paramType = "header"),
+	  })
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -52,6 +78,21 @@ public class AuditResource {
 		return Response.status(201).build();
 	}
 	
+	@ApiOperation(value="Gets audit records for a project")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Audit records successfully returned.", response=Audit.class, responseContainer="List"),
+		    @ApiResponse(code = 400, message = "Invalid ID supplied."),
+		    @ApiResponse(code = 403, message = "Not authorized to view audit records for this project."),
+		    @ApiResponse(code = 404, message = "Project not found."),
+		    @ApiResponse(code = 410, message = "Project has been deleted."),
+	})
+	// 
+	// These are used by upstream gateways; including them here makes it easier to use an API portal
+	//
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "Version", value = "API version number", required = false, dataType = "string", paramType = "header"),
+	    @ApiImplicitParam(name = "Authorization", value = "API key", required = false, dataType = "string", paramType = "header"),
+	  })
 	@GET
 	@Path("/project/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -68,6 +109,20 @@ public class AuditResource {
 		return Response.ok(AuditService.Factory.getInstance().getAuditListForProject(id)).build();
 	}
 	
+	@ApiOperation(value="Gets audit records for a user")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Audit records successfully returned.", response=Audit.class, responseContainer="List"),
+		    @ApiResponse(code = 400, message = "Invalid ID supplied."),
+		    @ApiResponse(code = 403, message = "Not authorized to view audit records for this user."),
+		    @ApiResponse(code = 404, message = "User not found.")
+	})
+	// 
+	// These are used by upstream gateways; including them here makes it easier to use an API portal
+	//
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "Version", value = "API version number", required = false, dataType = "string", paramType = "header"),
+	    @ApiImplicitParam(name = "Authorization", value = "API key", required = false, dataType = "string", paramType = "header"),
+	  })
 	@GET
 	@Path("/user/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
